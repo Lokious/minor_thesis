4adfc976482a6c4bb5581c3d02d66212d2bf1ff2
yingjie <mary2001@hp2021>
2023-02-09 01:18:50 +0100

    update

--- New folder/script/simulated_data.py
+++ New folder/script/simulated_data.py
@@ -7,36 +7,51 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 def generate_data():
-    # create time point(X)
-    time = np.array([x for x in range(0, 60)])
-    #print(time)
+    """
+    create 3D input dataset, with 41 time steps, 418 genotypes and 3 features
+    :return: list of dataframe
+    """
 
-    # create Y based on logistic function
-    Y = []
-    ## inilize parameters
-    k = random.uniform(0.3, 0.5)
-    L = random.randint(30, 60)
-    T = 30
-    #print(k, L, T)
-    for t in time:
-        Y.append(L / (1 + np.exp(-k * (t - T))))
+    features = ['trait_1','trait_2','traits_3']
+    genoypes = 418
+    time_step = 41
+    time_series = []
 
-    # add gaussian noise
-    Y = np.array([(y + random.uniform(0, 3)) for y in Y])
+    for feature in features:
+        # create time point(X)
+        feature_df = pd.DataFrame()
+        for genotype in range(genoypes):
+            time = np.array([x for x in range(0, time_step)])
+            #print(time)
 
-    # plt.scatter(time, Y)
-    # plt.show()
-    time_series = pd.DataFrame(data=Y)
-    #print(time_series)
+            # create Y based on logistic function
+            Y = []
+            ## inilize parameters
+            k = random.uniform(0.3, 0.5)
+            L = random.randint(30, 60)
+            T = 30
+            #print(k, L, T)
+            for t in time:
+                # calculate Y and also add gaussian noise
+                Y.append((L / (1 + np.exp(-k * (t - T))))+ random.uniform(0, 1))
+
+
+            #Y = np.array([(y + random.uniform(0, 3)) for y in Y])
+            Y = pd.DataFrame(data=Y,index=range(time_step),columns=[genotype])
+            feature_df = pd.concat([feature_df,Y],axis=1)
+            # plt.scatter(time, Y)
+            # plt.show()
+
+        time_series.append(feature_df)
+        #print(time_series)
     return time_series
 
 
 def return_simulated_dataset():
-    input_data = pd.DataFrame()
-    for i in range(418):
-        new_column = generate_data()
-        input_data = pd.concat([input_data,new_column],axis=1)
-    input_data.columns=list(range(0,418))
+
+
+    input_data = generate_data()
+
     return input_data
 
 
