39bcff1638e96b102012a73122693bc363201f52
yingjie <mary2001@hp2021>
2023-02-09 01:18:50 +0100

    update

--- New folder/script/atoencoder.py
+++ New folder/script/atoencoder.py
@@ -1,155 +1,223 @@
 import torch
 from torch import nn
+import torch.optim as optim
+
 from torch.nn import functional as F
 import numpy as np
 import random
 import matplotlib.pyplot as plt
 import copy
+import time
 from simulated_data import return_simulated_dataset
 from sklearn.model_selection import GroupShuffleSplit
 # set device: cup or GPU
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+hidden_size=128
 
-class Encoder(nn.Module):
+# # Define the LSTM autoencoder model
+class LSTMAutoencoder(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, dropout):
+        super(LSTMAutoencoder, self).__init__()
 
-    # 418 genes in DH_line, include drop out in encoder
-    def __init__(self, seq_length,features_num=418, hidden_size=128, num_layers=2, dropout=0):
-        super(Encoder, self).__init__()
-        self.seq_len = seq_length
-        self.n_features = features_num
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        # https://pytorch.org/docs/stable/generated/torch.nn.LSTM.html
-        self.lstm = nn.LSTM(
-            features_num,
-            hidden_size,
-            num_layers,
-            batch_first=True,
-            bidirectional=False,dropout=dropout
-        )
+        self.encoder = nn.LSTM(input_size, hidden_size, num_layers,
+                               batch_first=True, dropout=dropout)
+        self.decoder = nn.LSTM(hidden_size, input_size, num_layers,
+                               batch_first=True)
 
     def forward(self, x):
-        # x: tensor of shape (batch_size, seq_length, hidden_size)
-        x = x.reshape((1, self.seq_len, self.n_features))
-        encoder_outputs, (hidden, cell) = self.lstm(x)
+        x, _ = self.encoder(x)
+        x, _ = self.decoder(x)
+        return x
+#Define the Encoder class
+class Encoder(torch.nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, dropout=0.0):
+        super(Encoder, self).__init__()
+        self.lstm = torch.nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, dropout=dropout)
 
-        return (hidden, cell)
+    def forward(self, x):
+        _, (hidden, cell) = self.lstm(x)
+        return hidden, cell
 
-
-class Decoder(nn.Module):
-    def __init__(
-        self, feature_num=418, hidden_size=128, output_size=512, num_layers=2
-    ):
+# Define the Decoder class
+class Decoder(torch.nn.Module):
+    def __init__(self, hidden_size, output_size, num_layers, dropout=0.0):
         super(Decoder, self).__init__()
-        self.hidden_size = hidden_size
-        self.output_size = output_size
-        self.num_layers = num_layers
-        self.lstm = nn.LSTM(
-            feature_num,
-            hidden_size,
-            num_layers,
-            batch_first=True,
-            bidirectional=False
-        )
-        self.fc = nn.Linear(hidden_size, output_size)
+        self.lstm = torch.nn.LSTM(hidden_size, output_size, num_layers, batch_first=True, dropout=dropout)
 
-    def forward(self, x, hidden):
-        # x: tensor of shape (batch_size, seq_length, hidden_size)
-        decoder_output, (hidden, cell) = self.lstm(x, hidden)
-        prediction = self.fc(decoder_output)
-        return prediction, (hidden, cell)
+    def forward(self, hidden, cell, seq_len):
+        decoder_input = torch.zeros(hidden.shape[0], seq_len, hidden_size)
+        decoded, _ = self.lstm(decoder_input, (hidden, cell))
+        return decoded
 
+# Define the Autoencoder class
+class Autoencoder(torch.nn.Module):
+    """
+    Error !!
+    """
+    def __init__(self, input_size, hidden_size, num_layers, dropout=0.0):
+        super(Autoencoder, self).__init__()
+        self.encoder = Encoder(input_size, hidden_size, num_layers, dropout)
+        self.decoder = Decoder(hidden_size, input_size, num_layers, dropout)
 
-class LSTM_AE(nn.Module):
+    def forward(self, x):
+        hidden, cell = self.encoder(x)
+        decoded = self.decoder(hidden, cell, x.shape[1])
+        return decoded
 
-    def __init__(self,encoder,decoder):
-        super().__init__()
-        self.encoder = encoder
-        self.decoder = decoder
+def model_training(model,num_epochs,train_loader,test_dataset):
 
-    def forward(self, enc_X, dec_X, *args):
-        # X: tensor of shape (batch_size, seq_length, hidden_size)
-        enc_outputs = self.encoder(enc_X, *args)
-        dec_state = self.decoder.init_state(enc_outputs, *args)
-        return self.decoder(dec_X, dec_state)
-
-
-def train_model(model, train_dataset, val_dataset, n_epochs):
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-    criterion = nn.L1Loss(reduction='sum').to(device)
-    history = dict(train=[], val=[])
-
-    best_model_wts = copy.deepcopy(model.state_dict())
-    best_loss = 10000.0
-
-    for epoch in range(1, n_epochs + 1):
-        model = model.train()
-
-        train_losses = []
-        for seq_true in train_dataset:
+    # Define the loss function and optimizer
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    # Training loop
+    for epoch in range(num_epochs):
+        for i, data in enumerate(train_loader):
+            inputs = data
             optimizer.zero_grad()
-
-            seq_true = seq_true.to(device)
-            seq_pred = model(seq_true)
-
-            loss = criterion(seq_pred, seq_true)
-
+            outputs = model(inputs)
+            loss = criterion(outputs, inputs)
             loss.backward()
             optimizer.step()
 
-            train_losses.append(loss.item())
+        if (epoch + 1) % 10 == 0:
+            print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch + 1, num_epochs,
+                                                       loss.item()))
 
-        val_losses = []
-        model = model.eval()
-        with torch.no_grad():
-            for seq_true in val_dataset:
-                seq_true = seq_true.to(device)
-                seq_pred = model(seq_true)
-
-                loss = criterion(seq_pred, seq_true)
-                val_losses.append(loss.item())
-
-        train_loss = np.mean(train_losses)
-        val_loss = np.mean(val_losses)
-
-        history['train'].append(train_loss)
-        history['val'].append(val_loss)
-
-        if val_loss < best_loss:
-            best_loss = val_loss
-            best_model_wts = copy.deepcopy(model.state_dict())
-
-        print(f'Epoch {epoch}: train loss {train_loss} val loss {val_loss}')
-
-    model.load_state_dict(best_model_wts)
-    return model.eval(), history
-def main():
-    #generate simulated  dataset
+    # Test the autoencoder model on the test set
+    with torch.no_grad():
+        test_inputs = test_dataset
+        test_outputs = model(test_inputs)
+        test_loss = criterion(test_outputs, test_inputs)
+        print('Test Loss: {:.4f}'.format(test_loss.item()))
+def data_prepare():
+    """
+    logistic growth curve
+    :return: tensor
+    """
+    # generate simulated  dataset
     simulated_dataset = return_simulated_dataset()
-    print(simulated_dataset)
-    #number of days
-    time_step = 60
-    embedding_size = 418
-    num_hiddens = 128
+    simulated_dataset = simulated_dataset
+
+    def create_dataset(dfs):
+        #(41,418,3)
+        datasets = []
+        for df in dfs:
+
+            sequences = df.astype(np.float32).to_numpy().tolist()
+
+            dataset = torch.stack([torch.tensor(s).unsqueeze(1).float() for s in sequences])
+            datasets.append(dataset)
+        #remove the last dimention
+        tensor_dataset = torch.squeeze(torch.stack(datasets,dim=0))
+        print(tensor_dataset.shape)
+
+        n_features,seq_len,n_seq= tensor_dataset.shape
+        ##################
+        #seq_len:time step=60; n_features=3
+
+        return tensor_dataset, seq_len, n_features
+
+    dataset, seq_len, n_features = create_dataset(simulated_dataset)
+
+    #put time step at the first dimention
+    dataset = torch.permute(dataset, (2, 1, 0))#torch.Size([418, 41, 3])
+    #print(dataset.shape)
+    return dataset
+def generate_dataset2():
+    # Generate random simulated plant trait data
+    np.random.seed(0)
+    num_plants = 418 #genotypes
+    num_days = 41 #time steps
+    num_features = 3 #traits
+
+    days = np.arange(num_days)
+    X = np.zeros((num_plants, num_days, num_features))
+    for i in range(num_plants):
+        for j in range(num_features):
+            X[i, :, j] = np.sin(
+                2 * np.pi * days / num_days + 2 * np.pi * j / num_features) + np.random.normal(
+                0, 0.1, num_days)
+    # Convert the numpy array to tensors
+    X_tensor = torch.tensor(X, dtype=torch.float32)
+    return X_tensor
+def main():
+
+    num_plants = 418 #genotypes
+    num_days = 41 #time steps
+    num_features = 3 #traits
+    #simulated dataset 1
+    X_tensor = data_prepare()
+    # #simulated dataset 2
+    # X_tensor = generate_dataset2()
+
+    print(X_tensor.shape)
+    # Shuffle the dataset
+    indices = torch.randperm(num_plants)
+    X_tensor = X_tensor[indices]
+
+    # Split the dataset into training and test sets
+    train_ratio = 0.8
+    train_size = int(train_ratio * num_plants)
+    train_dataset = X_tensor[:train_size, :, :]
+    test_dataset = X_tensor[train_size:, :, :]
+
+    # Set the batch size
+    batch_size = 10
+
+    # Create the dataloader for the training dataset
+    train_dataloader = torch.utils.data.DataLoader(train_dataset,
+                                                   batch_size=batch_size,
+                                                   shuffle=True)
+
+    # Create the dataloader for the test dataset
+    test_dataloader = torch.utils.data.DataLoader(test_dataset,
+                                                  batch_size=batch_size,
+                                                  shuffle=False)
+
+    # Create the LSTM autoencoder model
+    input_size = 3
+    hidden_size = 128
+    num_epochs = 100
     num_layers = 2
-    dropout = 0.1
-    encoder = Encoder(time_step, embedding_size, num_hiddens, num_layers, dropout)
-    decoder = Decoder(embedding_size, num_hiddens, num_layers)
-    net = LSTM_AE(encoder, decoder)
+    model = LSTMAutoencoder(input_size, hidden_size, num_layers,dropout=0.2)
 
-    def create_dataset(df):
-        sequences = df.astype(np.float32).to_numpy().tolist()
-        dataset = [torch.tensor(s).unsqueeze(1).float() for s in sequences]
-        print(dataset) #? the last one is empty
-        n_seq, seq_len, n_features = torch.stack(dataset).shape
+    # Set the model to the GPU
+    #model = model.cuda() if torch.cuda.is_available() else model
 
-        return dataset, seq_len, n_features
-
-    train_df = simulated_dataset.sample(frac=0.8, random_state=200)
-    test_df = train_df.drop(train_df.index)
-    train_dataset, seq_len, n_features = create_dataset(train_df)
-    test_dataset, seq_len, n_features = create_dataset(test_df)
-    train_model(net, train_dataset, test_dataset, 2)
+    # #Define the loss function and optimizer
+    # criterion = nn.MSELoss()
+    # optimizer = optim.Adam(model.parameters(), lr=0.01)
+    model_training(model, num_epochs, train_dataloader, test_dataset)
+    #
+    # # Train the LSTM autoencoder model
+    #
+    # for epoch in range(num_epochs):
+    #     running_loss = 0.0
+    #     for i, data in enumerate(train_dataloader):
+    #         inputs = data.cuda() if torch.cuda.is_available() else data
+    #
+    #         optimizer.zero_grad()
+    #
+    #         outputs = model(inputs)
+    #         loss = criterion(outputs, inputs)
+    #         loss.backward()
+    #         optimizer.step()
+    #
+    #         running_loss += loss.item()
+    #
+    #     print("Epoch [{}/{}], Loss: {:.4f}".format(epoch + 1, num_epochs,
+    #                                                running_loss))
+    #
+    # # Test the LSTM autoencoder model
+    # with torch.no_grad():
+    #     running_loss = 0.0
+    #     for i, data in enumerate(test_dataloader):
+    #         inputs = data.cuda() if torch.cuda.is_available() else data
+    #         outputs = model(inputs)
+    #         loss = criterion(outputs, inputs)
+    #         running_loss += loss.item()
+    #
+    #     print("Test Loss: {:.4f}".format(running_loss))
 
 if __name__ == '__main__':
     main()
--- New folder/script/time_series_clustering.py
+++ New folder/script/time_series_clustering.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-
+from tslearn.clustering import TimeSeriesKMeans
 
 def read_and_reformat(file:str):
     platform_data = pd.read_csv(file,header=0,index_col=0)
     platform_data = platform_data.fillna(0.0)
-    print(len(platform_data["genotype_name"].unique()))
-    #print(platform_data)
+    # print(len(platform_data["genotype_name"].unique()))
+    # print(platform_data)
     platform_data.to_csv("../data/image_DHline_data_after_average_based_on_day.csv")
     groups_object = platform_data.groupby('plantid')
     group_number = len(groups_object.groups)
@@ -50,33 +50,181 @@ def read_and_reformat(file:str):
     # print(new_df_LA)
     # print(new_df_Height)
     # print(new_df_Biomass)
+    #use the average to fill in the NA
+    print(time_list)
+    for i in range(len(time_list)):
+        print(i)
+        for column in range(len(plantid_list)):
+            if (pd.isna(new_df_LA.iloc[i,column])):
+                try:
+                    if i == 0:
+                        # if the NA at the first time step
+                        new_df_LA.iloc[i, column] = 0
+                    elif i == len(time_list)-1:
+                        # if the NA at the last time step
+                        new_df_LA.iloc[i, column] = new_df_LA.iloc[i - 1, column]
+                    else:
 
-    new_df_LA.to_csv("../data/df_LA.csv")
-    new_df_Height.to_csv("../data/df_Height.csv")
-    new_df_Biomass.to_csv("../data/df_Biomass.csv")
-    return  new_df_LA,new_df_Height,new_df_Biomass
+                        print("column {}".format(column))
+                        move_step = 1
+                        if (pd.isna(new_df_LA.iloc[i-move_step,column]) or pd.isna(new_df_LA.iloc[i+move_step,column]))==False:
+                            print(new_df_LA.iloc[i-move_step,column],new_df_LA.iloc[i+move_step,column])
+                            average_value = (new_df_LA.iloc[i-move_step,column] + new_df_LA.iloc[i+move_step,column])/2
+                            print("average {}".format(average_value))
+                            new_df_LA.iloc[i,column] = average_value
+                        else:
+                            raise ValueError("nan in the besides cell(s)")
+                except:
+                    # if there are two or more continues NA
+                    print("not the first nor the last")
+                    while (i+move_step < len(time_list)-1):
+                        try:
+                            move_step += 1
+                            print(move_step+i)
+                            new_df_LA.iloc[i, column] = new_df_LA.iloc[
+                            i - 1, column] + ((new_df_LA.iloc[i + move_step, column] - new_df_LA.iloc[
+                            i - 1, column]) / (move_step+1))
+                            new_df_LA.iloc[i+1, column] = new_df_LA.iloc[
+                            i - 1, column] + 2*((new_df_LA.iloc[i + move_step, column] - new_df_LA.iloc[
+                            i - 1, column]) / (move_step+1))
+                            break
+                        except:
+                            continue
+                    else:
+                        print("all following values are na, fill in the following cell with the last non-na value")
+                        move_step = 1
+                        while (i-move_step) > 0:
+                            if pd.isna(new_df_LA.iloc[i - move_step, column])==False:
+                                new_df_LA.iloc[i, column] = new_df_LA.iloc[i - move_step, column]
+                                break
+                            else:
+                                move_step +=1
+                                continue
+                    print("fill with average")
+    for i in range(len(time_list)):
+        print(i)
+        for column in range(len(plantid_list)):
+            if (pd.isna(new_df_Height.iloc[i,column])):
+                try:
+                    if i == 0:
+                        # if the NA at the first time step
+                        new_df_Height.iloc[i, column] = 0
+                    elif i == len(time_list)-1:
+                        # if the NA at the last time step
+                        new_df_Height.iloc[i, column] = new_df_Height.iloc[i - 1, column]
+                    else:
+
+                        print("column {}".format(column))
+                        move_step = 1
+                        if (pd.isna(new_df_Height.iloc[i-move_step,column]) or pd.isna(new_df_Height.iloc[i+move_step,column]))==False:
+                            print(new_df_Height.iloc[i-move_step,column],new_df_Height.iloc[i+move_step,column])
+                            average_value = (new_df_Height.iloc[i-move_step,column] + new_df_Height.iloc[i+move_step,column])/2
+                            print("average {}".format(average_value))
+                            new_df_Height.iloc[i,column] = average_value
+                        else:
+                            raise ValueError("nan in the besides cell(s)")
+                except:
+                    # if there are two or more continues NA
+                    print("not the first nor the last")
+                    while (i+move_step < len(time_list)-1):
+                        try:
+                            move_step += 1
+                            print(move_step+i)
+                            new_df_Height.iloc[i, column] = new_df_Height.iloc[
+                            i - 1, column] + ((new_df_Height.iloc[i + move_step, column] - new_df_Height.iloc[
+                            i - 1, column]) / (move_step+1))
+                            new_df_Height.iloc[i+1, column] = new_df_Height.iloc[
+                            i - 1, column] + 2*((new_df_Height.iloc[i + move_step, column] - new_df_Height.iloc[
+                            i - 1, column]) / (move_step+1))
+                            break
+                        except:
+                            continue
+                    else:
+                        print("all following values are na, fill in the following cell with the last non-na value")
+                        move_step = 1
+                        while (i-move_step) > 0:
+                            if pd.isna(new_df_Height.iloc[i - move_step, column])==False:
+                                new_df_Height.iloc[i, column] = new_df_Height.iloc[i - move_step, column]
+                                break
+                            else:
+                                move_step +=1
+                                continue
+                    print("fill with average")
+    for i in range(len(time_list)):
+        print(i)
+        for column in range(len(plantid_list)):
+            if (pd.isna(new_df_Biomass.iloc[i,column])):
+                try:
+                    if i == 0:
+                        # if the NA at the first time step
+                        new_df_Biomass.iloc[i, column] = 0
+                    elif i == len(time_list)-1:
+                        # if the NA at the last time step
+                        new_df_Biomass.iloc[i, column] = new_df_Biomass.iloc[i - 1, column]
+                    else:
+
+                        print("column {}".format(column))
+                        move_step = 1
+                        if (pd.isna(new_df_Biomass.iloc[i-move_step,column]) or pd.isna(new_df_Biomass.iloc[i+move_step,column]))==False:
+                            print(new_df_Biomass.iloc[i-move_step,column],new_df_Biomass.iloc[i+move_step,column])
+                            average_value = (new_df_Biomass.iloc[i-move_step,column] + new_df_Biomass.iloc[i+move_step,column])/2
+                            print("average {}".format(average_value))
+                            new_df_Biomass.iloc[i,column] = average_value
+                        else:
+                            raise ValueError("nan in the besides cell(s)")
+                except:
+                    # if there are two or more continues NA
+                    print("not the first nor the last")
+                    while (i+move_step < len(time_list)-1):
+                        try:
+                            move_step += 1
+                            print(move_step+i)
+                            new_df_Biomass.iloc[i, column] = new_df_Biomass.iloc[
+                            i - 1, column] + ((new_df_Biomass.iloc[i + move_step, column] - new_df_Biomass.iloc[
+                            i - 1, column]) / (move_step+1))
+                            new_df_Biomass.iloc[i+1, column] = new_df_Biomass.iloc[
+                            i - 1, column] + 2*((new_df_Biomass.iloc[i + move_step, column] - new_df_Biomass.iloc[
+                            i - 1, column]) / (move_step+1))
+                            break
+                        except:
+                            continue
+                    else:
+                        print("all following values are na, fill in the following cell with the last non-na value")
+                        move_step = 1
+                        while (i-move_step) > 0:
+                            if pd.isna(new_df_Biomass.iloc[i - move_step, column])==False:
+                                new_df_Biomass.iloc[i, column] = new_df_Biomass.iloc[i - move_step, column]
+                                break
+                            else:
+                                move_step +=1
+                                continue
+                    print("fill with average")
+
+    print(new_df_LA)
+    # new_df_LA.to_csv("../data/df_LA.csv")
+    # new_df_Height.to_csv("../data/df_Height.csv")
+    # new_df_Biomass.to_csv("../data/df_Biomass.csv")
+    return new_df_LA,new_df_Height,new_df_Biomass
+
 def clusterinf(data_df):
-    from tslearn.clustering import TimeSeriesKMeans
-    print(data_df)
-    data_df.reset_index(inplace=True,drop=True)
-    print(data_df)
-    model = TimeSeriesKMeans(n_clusters=418, metric="dtw", max_iter=10)
-    model.fit(data_df.T)
-    y = model.predict(data_df.T)
-    print(y)
-    y = pd.DataFrame(data=y)
+
+    #print(data_df)
+    #data_df = data_df["Biomass_Estimated_log_transformed","Height_Estimated_log_transformed","LA_Estimated_log_transformed"]
+    # 418 genotypes
+    from sklearn.cluster import KMeans
+    model = KMeans(n_clusters=3,n_init=20)
+
+    y = model.fit_predict(data_df.T)
+    y = pd.DataFrame(data=y,columns=["predict"],index=list(range(1200)))
+    # print(y)
+    # print(y["predict"].unique())
     y.to_csv("clustering_result.csv")
-
-
 
 def main():
     LA,Height,Biomass = read_and_reformat(file="../data/image_DHline_data_after_average_based_on_day.csv")
-    from preprocessing import plot_raw_data
-
-    df = pd.read_csv("../data/image_DHline_data_after_average_based_on_day.csv",header=0,index_col=0)
-    df = df.dropna()
-    plot_raw_data(df)
-    #clusterinf(LA)
+    # print("LA")
+    # print(LA)
+    clusterinf(LA)
 
 
 
